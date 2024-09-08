d9f09210c313a57cb4e27b3e6f71ec9ce59d8339
MahmoudAyman <mahmoud.ayman07@gmail.com>
2024-09-08 14:54:23 +0300

    added app lifetime.py

---
+++ app/core/lifetime.py
@@ -0,0 +1,36 @@
+# app/core/lifetime.py
+
+from typing import Awaitable, Callable
+
+def register_startup_event(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:  # pragma: no cover
+    """
+    Actions to run on application startup.
+
+    :param app: the fastAPI application.
+    :return: event handler.
+    """
+
+    @app.on_event("startup")
+    async def _startup() -> None:
+        app.middleware_stack = None
+        app.middleware_stack = app.build_middleware_stack()
+
+    return _startup
+
+
+def register_shutdown_event(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:  # pragma: no cover
+    """
+    Actions to run on application's shutdown.
+
+    :param app: fastAPI application.
+    :return: event handler.
+    """
+
+    @app.on_event("shutdown")
+    async def _shutdown() -> None:
+        pass
+    return _shutdown
--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
+# app/main.py
+
 from fastapi import FastAPI
-from app.api.inference import inference_router
-from app.api.healthcheck import healthcheck_router
+#from app.core.config import settings  # Configuration module
+from app.api.inference import router as inference_router
+from app.api.healthcheck import router as healthcheck_router
+from app.core.lifetime import register_startup_event, register_shutdown_event
+import uvicorn
 
-app = FastAPI()
+# Create FastAPI app
+app = FastAPI(
+    title="Pest Identification ML API",
+    version="1.0.0"
+)
 
-app.include_router(inference_router)
-app.include_router(healthcheck_router)
+# Include API routes
+app.include_router(inference_router, prefix="/api/v1/inference", tags=["Inference"])
+app.include_router(healthcheck_router, prefix="/api/v1/healthcheck", tags=["Healthcheck"])
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the ML API"}
+#Register startup and shutdown events
+register_startup_event(app)
+register_shutdown_event(app)
+
+
+# Entry point for running the app directly (if not using a dedicated ASGI server)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
