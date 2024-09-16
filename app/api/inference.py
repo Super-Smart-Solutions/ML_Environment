60b735461db74d9a63a041c113cfa3dc6f37c360
Ahmed Samy <Ahmed.samy18296@gmail.com>
2024-09-16 04:06:31 +0300

    Additional update on project
    This version is already tested and running very good

--- app/api/inference.py
+++ app/api/inference.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, HTTPException
 from app.schemas.request import InferenceRequest
+from app.schemas.response import InferenceResponse
 from app.services.model_inference import run_inference_service
 
 router = APIRouter()
 
-@router.post("/predict")
+@router.post("/predict", response_model=InferenceResponse)
 async def run_inference(request: InferenceRequest):
     try:
         result = await run_inference_service(request.model_name, request.presigned_url)
@@ -15,7 +16,7 @@ async def run_inference(request: InferenceRequest):
 @router.post("/reload_model/{model_name}")
 async def reload_model(model_name: str):
     try:
-        result = await run_inference_service.reload_model(model_name)
+        await run_inference_service.reload_model(model_name)
         return {"message": f"Model {model_name} reloaded successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
