f531bcabe8c9323bf20569a9cd9a03a489fe38da
MahmoudAyman <mahmoud.ayman07@gmail.com>
2024-09-21 13:09:59 +0300

    updated application.py

--- app/core/application.py
+++ app/core/application.py
@@ -1,4 +1,3 @@
-# type: ignore
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.inference import router as inference_router
@@ -6,7 +5,19 @@ from app.api.healthcheck import router as healthcheck_router
 from app.core.lifetime import register_startup_event, register_shutdown_event
 
 def get_app() -> FastAPI:
-    app = FastAPI(title="ML API")
+    """
+    Get FastAPI application.
+    This is the main constructor of an application.
+    :return: application.
+    """
+    app = FastAPI(
+        title="Pest Identification ML API",
+        version="1.0.0",
+    )
+    
+    # Register app startup and shutdown events
+    register_startup_event(app)
+    register_shutdown_event(app)
     
     # CORS setup
     app.add_middleware(
@@ -18,11 +29,9 @@ def get_app() -> FastAPI:
     )
 
     # Include routers
-    app.include_router(inference_router, prefix="/api")
-    app.include_router(healthcheck_router)
+    app.include_router(inference_router, prefix="/api/inference", tags=["Inference"])
+    app.include_router(healthcheck_router, prefix="/api/healthcheck", tags=["Healthcheck"])
 
-    # Register app startup and shutdown events
-    register_startup_event(app)
-    register_shutdown_event(app)
+    
 
     return app
