6f2a19fac28d021990a1cae6f9cca20441c3205b
MahmoudAyman <mahmoud.ayman07@gmail.com>
2024-09-21 13:14:51 +0300

    restored lifetime.py

--- app/core/lifetime.py
+++ app/core/lifetime.py
@@ -1,11 +1,30 @@
+from typing import Awaitable, Callable
 from fastapi import FastAPI
 
-def register_startup_event(app: FastAPI):
+def register_startup_event(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:  # pragma: no cover
+    """
+    Actions to run on application startup.
+    :param app: the fastAPI application.
+    :return: event handler.
+    """
     @app.on_event("startup")
-    async def startup_event():
-        print("Application startup")
+    async def _startup() -> None:
+        app.middleware_stack = None
+        app.middleware_stack = app.build_middleware_stack()
+    return _startup
 
-def register_shutdown_event(app: FastAPI):
+
+def register_shutdown_event(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:  # pragma: no cover
+    """
+    Actions to run on application's shutdown.
+    :param app: fastAPI application.
+    :return: event handler.
+    """
     @app.on_event("shutdown")
-    async def shutdown_event():
-        print("Application shutdown")
+    async def _shutdown() -> None:
+        pass
+    return _shutdown
