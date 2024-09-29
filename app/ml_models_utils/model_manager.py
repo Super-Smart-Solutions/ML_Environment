0a3db9873f6e18984c4089a52ee974c75530730d
MahmoudAyman <mahmoud.ayman07@gmail.com>
2024-09-29 17:01:13 +0300

    fixed model laoding issue

--- app/ml_models_utils/model_manager.py
+++ app/ml_models_utils/model_manager.py
@@ -1,44 +1,14 @@
 import os
 import aioboto3
-# import tensorflow as tf
-# from tensorflow import keras
-from app.core.config import settings  # Import settings for AWS credentials
+from app.core.config import settings
 from fastapi import HTTPException
 from functools import lru_cache
 from app.utils.s3_utils import s3_download_object_decorator
 import asyncio
 
-# import numpy as np
-# import keras
-# It can be used to reconstruct the model identically.
-# reconstructed_model = keras.models.load_model(r"C:\Users\ahmed\sss_models\Mango_best_model_weights.keras")
-# print(reconstructed_model)
-# from tensorflow.keras.models import load_model
-# from tensorflow.keras.optimizers import Adam
-# from tensorflow.keras.metrics import Precision, Recall, AUC
-# from tensorflow.keras.layers import LayerNormalization
-# from tensorflow.keras import backend as K
-
-# K.clear_session()
-# # Define a dictionary of custom objects
-# custom_objects = {
-#     'Adam': Adam,
-#     'Precision': Precision,
-#     'Recall': Recall,
-#     'AUC': AUC,
-#     'LayerNormalization': LayerNormalization
-# }
 import numpy as np
-# import tensorflow as tf
-# from tensorflow import keras
-# from keras import layers
-# import keras
-from tensorflow import keras
 from tensorflow.keras.models import load_model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.metrics import Precision, Recall, AUC
-from tensorflow.keras.layers import LayerNormalization
-from tensorflow.keras import backend as K
+
 
 
 class ModelManager:
@@ -53,14 +23,14 @@ class ModelManager:
         """
 
         # Define the path for the model file
-        model_file_path = os.path.join(self.model_directory,"models",f"{model_name}", f"{model_name}_model.keras")
+        model_file_path = os.path.join(self.model_directory,"models",f"{model_name}", f"{model_name}_model.h5")
 
         # Check if model file exists locally
         if os.path.exists(model_file_path):
-            print(f"Loading model from local file: {model_file_path}")
-            return keras.models.load_model(model_file_path)
+            #print(f"Loading model from local file: {model_file_path}")
+            return load_model(model_file_path)
         else:
-            print(f"File not found: {model_file_path}")
+            #print(f"File not found: {model_file_path}")
             raise FileNotFoundError(f"Model {model_name} not found at {model_file_path}")
 
 
@@ -79,9 +49,8 @@ class ModelManager:
             return True if os.path.exists(downloaded_file_path) else False
 
 
-        # model_key = f"models/{model_name}/{model_name}_model.keras"
-        # result = await _get_model(object_key=model_key)
-        result = True
+        model_key = f"models/{model_name}/{model_name}_model.h5"
+        result = await _get_model(object_key=model_key)
 
         if result:
             # Load the model from the downloaded file
