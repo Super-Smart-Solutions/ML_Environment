020cf45c261138212a5a1547c595e1b0fbd8bc26
MahmoudAyman <mahmoud.ayman07@gmail.com>
2024-10-05 13:02:37 +0300

    Fixed config

--- app/core/config.py
+++ app/core/config.py
@@ -35,21 +35,25 @@ class Settings(BaseSettings):
     log_level: LogLevel = LogLevel.INFO
 
     #AWS S3
-    AWS_DEFAULT_REGION: str = Fie
-    AWS_ACCESS_KEY_ID: str = ""
-    AWS_SECRET_ACCESS_KEY: str = ""
-    AWS_WEIGHTS_BUCKET_NAME: str = ""
-    AWS_IMAGES_BUCKET_NAME: str = ""
+    AWS_DEFAULT_REGION: str = Field(..., env="AWS_DEFAULT_REGION")
+    AWS_ACCESS_KEY_ID: str = Field(..., env="AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY: str = Field(..., env="AWS_SECRET_ACCESS_KEY")
+    AWS_WEIGHTS_BUCKET_NAME: str = Field(..., env="AWS_WEIGHTS_BUCKET_NAME")
+    AWS_IMAGES_BUCKET_NAME: str = Field(..., env="AWS_IMAGES_BUCKET_NAME")
 
     #Weights [absolute path]
     WEIGHTS_DIR: str = os.path.join(os.path.dirname(__file__), "../weights")
     VERSIONS_PATH: str = os.path.join(os.path.dirname(__file__), "../weights/versions.json")
 
-    model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(__file__), "../.env"),
-        env_prefix="ML_APIS_",
-        env_file_encoding="utf-8",
-    )
+    # model_config = SettingsConfigDict(
+    #     env_file=os.path.join(os.path.dirname(__file__), "../.env"),
+    #     env_prefix="ML_APIS_",
+    #     env_file_encoding="utf-8",
+    # )
+
+    class Config:
+        env_prefix = "ML_APIS_"
+        case_sensitive = True
 
 # Instantiate the settings
 settings = Settings()
